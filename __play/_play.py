539e71646913ee1b6d0b66240040dcf8dad08ceb
johnashurst-sg <73337262+johnashurst-sg@users.noreply.github.com>
2022-07-05 18:32:00 +0200

    [FIX] rounding

--- __play/_play.py
+++ __play/_play.py
@@ -3,5 +3,5 @@ from decimal import *
 decimals = 4
 places = '.' + "".join(["0" for _ in range(decimals)])
 print(places)
-res = Decimal('10000.00006').quantize(Decimal(places), rounding=ROUND_DOWN)
-print(res)
+res = Decimal('10000.12346').quantize(Decimal(places), rounding=ROUND_DOWN)
+print(float(res))
